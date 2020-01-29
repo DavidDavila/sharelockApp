4205b1cce22cfeffb79829b09d9e58aa9b93fabf
David <david.davila@electronicid.eu>
2020-01-29 18:36:08 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [ AppComponent, StartComponent ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+  imports: [ BrowserModule, IonicModule.forRoot(), AppRoutingModule ],
+  providers: [ StatusBar, SplashScreen, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy } ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule {}
---
+++ src/app/start/start.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './start.component.html',
+  styleUrls: [ './start.component.scss' ]
+})
+export class StartComponent implements OnInit {
+  public actualSlide = 1;
+  constructor() {}
+
+  ngOnInit() {}
+}
